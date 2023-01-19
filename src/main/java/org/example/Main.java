8cb3e50db23ba9c31b7f5310276d05e477172a4f
Kapil Kaushik <kapil.kaushik@cavisson.com>
2023-01-19 16:56:24 +0530

    add some code to Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,14 @@
 package org.example;
 
+import java.util.function.Function;
+import java.util.function.IntFunction;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Function<Integer,Integer> f = i -> i*i;
+        IntFunction<Integer> ff = i->i*i;
+        System.out.println(f.apply(2)); // low perf
+        System.out.println(ff.apply(2)); // high perf
     }
 }
